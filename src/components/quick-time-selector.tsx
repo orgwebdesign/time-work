8c244e86ed4108bddbf626f080b23f4139215cc1
web.design org <orgwebdesign@gmail.com>
2025-06-28 19:48:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/components/quick-time-selector.tsx
+++ src/components/quick-time-selector.tsx
@@ -16,7 +16,7 @@ const timeOptions = [
 ];
 
 const hours = Array.from({ length: 24 }, (_, i) => i.toString().padStart(2, '0'));
-const minutes = ['00', '15', '30', '45'];
+const minutes = Array.from({ length: 12 }, (_, i) => (i * 5).toString().padStart(2, '0'));
 
 interface QuickTimeSelectorProps {
   value?: string;
